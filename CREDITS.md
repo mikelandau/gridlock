af606d2c50a4e0e1ceafd43ba6da881b480de166
Michael Landau <mnlandau90@gmail.com>
2024-08-08 15:32:32 -0400

    webpack init

---
+++ CREDITS.md
@@ -0,0 +1,3 @@
+## References
+- [Megida, Dillon](https://www.freecodecamp.org/news/author/dillionmegida/) - [HTML Starter Template – A Basic HTML5 Boilerplate for index.html](https://www.freecodecamp.org/news/html-starter-template-a-basic-html5-boilerplate-for-index-html/)
+- [Mohammadzadeh, Mostafa](https://stackoverflow.com/users/5328721/mostafa-mohammadzadeh) - [Push assets folder to public directory with webpack](https://stackoverflow.com/a/74011130)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "public", to: "" }
+      ],
+    }),
+  ],
+};
